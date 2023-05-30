5605b36eddd04f602b38aad20e5c96ffbc9c8de2
saooms <saooms@hotmail.com>
2023-05-30 17:41:48 +0200

    actually saved dispenser code

--- module-software/dispenser/dispenser.ino
+++ module-software/dispenser/dispenser.ino
@@ -1,24 +1,31 @@
+#include <Wire.h>
 #include "lib/dispenser.h"
 
-Dispenser dispensers[2] = {
+Dispenser dispensers[3] = {
     Dispenser(3),
-    Dispenser(4)
+    Dispenser(4),
+    Dispenser(7)
 };
 
-void setup(){
-    Serial.begin(9600);
-    dispensers[0].dispense();
-    dispensers[0].dispense();
-    dispensers[0].dispense();
-    dispensers[0].dispense();
+#define I2C_ADDRESS 0x3A
 
+void receiveHandle(int args){
+    while (Wire.available())
+    {
+        int target = Wire.read();
+        dispensers[target].dispense();
+    }
+}
+
+void setup(){
+    Wire.onReceive(receiveHandle);
+    Wire.begin(I2C_ADDRESS);
+    Serial.begin(9600);
 }
 
 void loop(){
-    for (size_t i = 0; i < 2; i++)
+    for (size_t i = 0; i < 3; i++)
     {
         dispensers[i].update();
     }
-   
-   
 }
