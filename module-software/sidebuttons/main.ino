634f817f03d2e8f8e61450517c9b3f13e4f1fa65
saooms <saooms@hotmail.com>
2023-05-30 16:27:55 +0200

    sync

--- module-software/sidebuttons/main.ino
+++ module-software/sidebuttons/main.ino
@@ -1,28 +1,30 @@
 #include <Wire.h>
 
 #define I2C_ADDRESS 0x2F
-const int BTN_PIN[] = {4,5,6,7,8,9,10,11};
+int BTN_PIN[] = {4,5,6,7,8,9,10,11};
+int result = 0;
 
 void requestHandle(){
-    int result = 0;
 
-    for (int i : BTN_PIN)
-        if (digitalRead(BTN_PIN[i]))
-            result = i;
-
+    Serial.println(result);
     Wire.write(result);
 }
 
 void setup(){
     for (int i : BTN_PIN)
     {
-        pinMode(BTN_PIN[i], INPUT_PULLUP);
+        pinMode(i, INPUT_PULLUP);
     }
+    Serial.begin(9600);
     
     Wire.begin(I2C_ADDRESS);
     Wire.onRequest(requestHandle);
 }
 
 void loop(){
+    result = 0;
 
+    for (int i : BTN_PIN)
+        if (!digitalRead(i))
+            result = i + 48;
 }
